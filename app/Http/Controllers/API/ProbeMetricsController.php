53261835b652e6c4c5f0d3fc5093005e41f0b95e
jean-marc strauven <jms@grazulex.be>
2024-06-10 01:48:38 +0200

    pint

---
+++ app/Http/Controllers/API/ProbeMetricsController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProbeMetrics;
+use Illuminate\Http\Request;
+
+class ProbeMetricsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProbeMetrics $probeMetrics)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ProbeMetrics $probeMetrics)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProbeMetrics $probeMetrics)
+    {
+        //
+    }
+}
---
+++ database/factories/ProbeMetricsFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ProbeMetrics;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProbeMetricsFactory extends Factory{
+    protected $model = ProbeMetrics::class;
+
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
