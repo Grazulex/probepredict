ef89812a39519f928ee74c080e4aecf59893208d
jean-marc strauven <jms@grazulex.be>
2024-06-30 07:31:13 +0200

    avoid double value

--- app/Actions/Metrics/CreateMetricAction.php
+++ app/Actions/Metrics/CreateMetricAction.php
@@ -11,6 +11,16 @@ class CreateMetricAction
 {
     public function handle(array $input): ProbeMetric
     {
+        $value = $input['value'];
+        $metric_type_id = $input['metric_type_id'];
+
+        $lastMetric = ProbeMetric::where('metric_type_id', $metric_type_id)
+            ->orderBy('created_at', 'desc')
+            ->first();
+        if ($lastMetric && $lastMetric->value === $value) {
+            return $lastMetric;
+        }
+
         $probeMetric = ProbeMetric::create($input);
 
         CalculateJob::dispatch(
